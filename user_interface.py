c56ff33524ab4c9ca0358aa12154569d3056f53e
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-20 00:01:42 +0800

    ui pseudocode

---
+++ user_interface.py
@@ -0,0 +1,26 @@
+#Christian James M. Luces | BSCpE 1-5 | Final Project - COVID Contact Tracing App
+
+#Import necessary modules
+#Create class
+#Saving User's Information Label
+#Get name
+#Get age
+#Get gender
+#Get address
+#Get contact number
+#Get email address
+
+#Saving User's Emergency Information Label
+#Get name of guardian
+#Get contact number of guardian
+#Get email address of guardian
+
+#User's Health Declaration Label
+#Get vaccine status
+#Get symptoms
+#Get COVID test result
+#Get travel history
+
+#Show introduction
+#Ask for User's consent
+
