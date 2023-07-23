01b10e248a122685c6cc2fa144e3fc73d5ee78d0
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-24 02:33:20 +0800

    get last place visited

--- contact_form.py
+++ contact_form.py
@@ -61,7 +61,7 @@ class TracingForm():
       entry_barangay = Entry(self.user_information_window)
       entry_barangay.grid(row=3, column=4)
       
-      city = Label(self.user_information_window, text="City:")
+      city = Label(self.user_information_window, text="City/Town:")
       city.grid(row=3, column=5, padx=5, pady=10)
       entry_city = Entry(self.user_information_window)
       entry_city.grid(row=3, column=6)
@@ -78,25 +78,43 @@ class TracingForm():
       entry_email = Entry(self.user_information_window)
       entry_email.grid(row=5, column=1)
 
-      #Get date
-      date = Label(self.user_information_window, text="Date is:", font=("Arial", 9, "bold"))
-      date.grid(row=6, column=0, padx=5, pady=10)
+      #Get Last Location
+      location = Label(self.user_information_window, text="Last Visited Place:", font=("Arial", 9, "bold"))
+      location.grid(row=6, column=0, padx=5, pady=10)
+
+      last_house_number_street_village = Label(self.user_information_window, text="House No./Street/Village:")
+      last_house_number_street_village.grid(row=6, column=1, padx=5, pady=10)
+      entry_last_house_number_street_village = Entry(self.user_information_window)
+      entry_last_house_number_street_village.grid(row=6, column=2)
+
+      last_barangay = Label(self.user_information_window, text="Barangay:")
+      last_barangay.grid(row=6, column=3, padx=5, pady=10)
+      entry_last_barangay = Entry(self.user_information_window)
+      entry_last_barangay.grid(row=6, column=4)
+
+      last_city = Label(self.user_information_window, text="City/Town:")
+      last_city.grid(row=6, column=5, padx=5, pady=10)
+      entry_last_city = Entry(self.user_information_window)
+      entry_last_city.grid(row=6, column=6)
+
+      #Get date for that location
+      date = Label(self.user_information_window, text="Date of visit:", font=("Arial", 9, "bold"))
+      date.grid(row=7, column=0, padx=5, pady=10)
 
       month = Label(self.user_information_window, text="Month:")
-      month.grid(row=6, column=1, padx=5, pady=10)
+      month.grid(row=7, column=1, padx=5, pady=10)
       entry_month = Spinbox(self.user_information_window, from_=0, to=12)
-      entry_month.grid(row=6, column=2, padx=5, pady=10)
+      entry_month.grid(row=7, column=2, padx=5, pady=10)
 
       day = Label(self.user_information_window, text="Day:")
-      day.grid(row=6, column=3, padx=5, pady=10)
+      day.grid(row=7, column=3, padx=5, pady=10)
       entry_day = Spinbox(self.user_information_window, from_=0, to=31)
-      entry_day.grid(row=6, column=4, padx=5, pady=10)
+      entry_day.grid(row=7, column=4, padx=5, pady=10)
 
       year = Label(self.user_information_window, text="Year:")
-      year.grid(row=6, column=5, padx=5, pady=10)
+      year.grid(row=7, column=5, padx=5, pady=10)
       entry_year= Spinbox(self.user_information_window, from_=2019, to=2023)
-      entry_year.grid(row=6, column=6, padx=5, pady=10)
-
+      entry_year.grid(row=7, column=6, padx=5, pady=10)
 
       #Get vaccine status
       vaccine_status = Label(self.user_information_window, text="COVID-19 Vaccination Status:", font=("Arial", 9, "bold"))
@@ -122,6 +140,8 @@ class TracingForm():
       second_booster_shot = Radiobutton(self.user_information_window, variable=radio, text="2nd Booster Shot", value=5)
       second_booster_shot.grid(row=8, column=5)
 
+      #Get COVID test result
+
 
       self.user_information_window.mainloop()
 
@@ -131,5 +151,3 @@ class TracingForm():
 #Get email address of guardian
 
 #Get symptoms
-#Get COVID test result
-#Get travel history 
