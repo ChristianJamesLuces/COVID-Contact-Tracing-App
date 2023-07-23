666bc940e259e56d1473bf6d9b811dbe7a5c35af
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-23 19:50:39 +0800

    create the main window

--- user_interface.py
+++ user_interface.py
@@ -1,11 +1,34 @@
 #Christian James M. Luces | BSCpE 1-5 | Final Project - COVID Contact Tracing App
 
 #Import necessary modules
+from tkinter import *
+
+
+
 #Create class
-#Saving User's Information Label
-#Get name
+class UserInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.main_window()
+        
+        self.window.mainloop()
+    #Create a main window
+    def main_window(self):
+        self.window.title("Covid Tracing App")
+        self.window.geometry("1250x400")
+        
+
+        label = Label(self.window, text = "Welcome to COVID Contact Tracing App", fg="black", font=("Arial", 15, "bold"))
+        label.place (x=780, y=20)
+
+
+    #Create a Contact Form button
+
+      
+    #Saving User's Information Label
+    #Get name
 #Get age
-#Get gender
+#Get sex
 #Get address
 #Get contact number
 #Get email address
