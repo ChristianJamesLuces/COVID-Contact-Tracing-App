7d6492ceba064d98543bc3f1cb74382ed3028323
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-24 01:53:17 +0800

    get date

--- contact_form.py
+++ contact_form.py
@@ -53,54 +53,74 @@ class TracingForm():
       
       house_number_street_village = Label(self.user_information_window, text="House No./Street/Village:")
       house_number_street_village.grid(row=3, column=1, padx=5, pady=10)
-      house_number_street_village = Entry(self.user_information_window)
-      house_number_street_village.grid(row=3, column=2)
+      entry_house_number_street_village = Entry(self.user_information_window)
+      entry_house_number_street_village.grid(row=3, column=2)
       
       barangay = Label(self.user_information_window, text="Barangay:")
       barangay.grid(row=3, column=3, padx=5, pady=10)
-      barangay = Entry(self.user_information_window)
-      barangay.grid(row=3, column=4)
+      entry_barangay = Entry(self.user_information_window)
+      entry_barangay.grid(row=3, column=4)
       
       city = Label(self.user_information_window, text="City:")
       city.grid(row=3, column=5, padx=5, pady=10)
-      city = Entry(self.user_information_window)
-      city.grid(row=3, column=6)
+      entry_city = Entry(self.user_information_window)
+      entry_city.grid(row=3, column=6)
       
       #Get contact number
       contact_number = Label(self.user_information_window, text="Contact No. (ex.09...):", font=("Arial", 9, "bold"))
       contact_number.grid(row=4, column=0, padx=5, pady=10)
-      contact_number = Entry(self.user_information_window)
-      contact_number.grid(row=4, column=1)
+      entry_contact_number = Entry(self.user_information_window)
+      entry_contact_number.grid(row=4, column=1)
       
       #Get email address
       email = Label(self.user_information_window, text="Email Address:", font=("Arial", 9, "bold"))
       email.grid(row=5, column=0, padx=5, pady=10)
-      email = Entry(self.user_information_window)
-      email.grid(row=5, column=1)
+      entry_email = Entry(self.user_information_window)
+      entry_email.grid(row=5, column=1)
+
+      #Get date
+      date = Label(self.user_information_window, text="Date is:", font=("Arial", 9, "bold"))
+      date.grid(row=6, column=0, padx=5, pady=10)
+
+      month = Label(self.user_information_window, text="Month:")
+      month.grid(row=6, column=1, padx=5, pady=10)
+      entry_month = Spinbox(self.user_information_window, from_=0, to=12)
+      entry_month.grid(row=6, column=2, padx=5, pady=10)
+
+      day = Label(self.user_information_window, text="Day:")
+      day.grid(row=6, column=3, padx=5, pady=10)
+      entry_day = Spinbox(self.user_information_window, from_=0, to=31)
+      entry_day.grid(row=6, column=4, padx=5, pady=10)
+
+      year = Label(self.user_information_window, text="Year:")
+      year.grid(row=6, column=5, padx=5, pady=10)
+      entry_year= Spinbox(self.user_information_window, from_=2019, to=2023)
+      entry_year.grid(row=6, column=6, padx=5, pady=10)
+
 
       #Get vaccine status
       vaccine_status = Label(self.user_information_window, text="COVID-19 Vaccination Status:", font=("Arial", 9, "bold"))
-      vaccine_status.grid(row=6, column=0, padx=5, pady=10)
+      vaccine_status.grid(row=8, column=0, padx=5, pady=10)
       
       radio = IntVar()
       not_yet = Radiobutton(self.user_information_window,variable=radio, text="Not Yet", value=1)
-      not_yet.grid(row=6, column=1)
+      not_yet.grid(row=8, column=1)
       
       
       first_dose = Radiobutton(self.user_information_window, variable=radio, text="1st Dose", value=2)
-      first_dose.grid(row=6, column=2)
+      first_dose.grid(row=8, column=2)
       
       
       second_dose = Radiobutton(self.user_information_window, variable=radio, text="2nd Dose", value=3)
-      second_dose.grid(row=6, column=3)
+      second_dose.grid(row=8, column=3)
 
       
       first_booster_shot = Radiobutton(self.user_information_window, variable=radio, text="1st Booster Shot", value=4)
-      first_booster_shot.grid(row=6, column=4)
+      first_booster_shot.grid(row=8, column=4)
 
       
       second_booster_shot = Radiobutton(self.user_information_window, variable=radio, text="2nd Booster Shot", value=5)
-      second_booster_shot.grid(row=6, column=5)
+      second_booster_shot.grid(row=8, column=5)
 
 
       self.user_information_window.mainloop()
@@ -110,8 +130,6 @@ class TracingForm():
 #Get contact number of guardian
 #Get email address of guardian
 
-
-#Get vaccine status
 #Get symptoms
 #Get COVID test result
 #Get travel history 
