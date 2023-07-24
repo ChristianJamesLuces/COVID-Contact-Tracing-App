4ec7eafb0637afdca87f7b40205900e1b7c497c3
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-25 00:22:31 +0800

    get last place visited

--- tracing_form.py
+++ tracing_form.py
@@ -15,10 +15,10 @@ class TracingForm():
         self.address()
         self.contact_number()
         self.email_adress()
+        self.last_place()
 
-    
+    #Get name
     def name(self):
-        #Get name
         self.first_name = tk.Label(self.user_information_window, text="First Name: ", font=("Arial", 9, "bold"), bg="#ECDADD")
         self.first_name.grid(row=0, column=0, padx=5, pady=10)
         self.entry_first_name = tk.Entry(self.user_information_window)
@@ -34,13 +34,14 @@ class TracingForm():
         self.entry_last_name = tk.Entry(self.user_information_window)
         self.entry_last_name.grid(row=0, column=5)
     
+    #Get age 
     def age(self):
-        #Get age 
         self.age_label = tk.Label(self.user_information_window, text="Age (yrs. old):", font=("Arial", 9, "bold"), bg="#ECDADD")
         self.age_label.grid(row=1, column=0, padx=5, pady=10)
         self.entry_age = tk.Entry(self.user_information_window)
         self.entry_age.grid(row=1, column=1)
     
+    #Get sex
     def sex(self):
         self.sex_label = tk.Label(self.user_information_window, text="Sex:", font=("Arial", 9, "bold"), bg="#ECDADD")
         self.sex_label.grid(row=2, column=0, padx=5, pady=10)
@@ -48,6 +49,7 @@ class TracingForm():
         self.sex_box = ttk.Combobox(self.user_information_window, values= ["Male", "Female", "Intersex"], width=8)
         self.sex_box.grid(row=2, column=1, padx=5, pady=10)
     
+    #Get adress
     def address(self):
         self.address_label = tk.Label(self.user_information_window, text="Address:", font=("Arial", 9, "bold"), bg="#ECDADD")
         self.address_label.grid(row=3, column=0, padx=5, pady=10)
@@ -67,14 +69,39 @@ class TracingForm():
         self.entry_city = tk.Entry(self.user_information_window)
         self.entry_city.grid(row=3, column=6)
 
+    #Get contact number
     def contact_number(self):
         self.contact_number_label = tk.Label(self.user_information_window, text="Contact No. (ex.09...):", font=("Arial", 9, "bold"), bg="#ECDADD")
         self.contact_number_label.grid(row=4, column=0, padx=5, pady=10)
         self.entry_contact_number = tk.Entry(self.user_information_window)
         self.entry_contact_number.grid(row=4, column=1)
 
+    #Get email adress
     def email_adress(self):
         self.email = tk.Label(self.user_information_window, text="Email Address:", font=("Arial", 9, "bold"), bg="#ECDADD")
         self.email.grid(row=5, column=0, padx=5, pady=10)
         self.entry_email = tk.Entry(self.user_information_window)
         self.entry_email.grid(row=5, column=1)
+    
+    #Get last location
+    def last_place(self):
+        self.location = tk.Label(self.user_information_window, text="Last Visited Place:", font=("Arial", 9, "bold"), bg="#ECDADD")
+        self.location.grid(row=6, column=0, padx=5, pady=10)
+        
+        self.last_house_number_street_village = tk.Label(self.user_information_window, text="House No./Street/Village:", bg="#ECDADD")
+        self.last_house_number_street_village.grid(row=6, column=1, padx=5, pady=10)
+        self.entry_last_house_number_street_village = tk.Entry(self.user_information_window)
+        self.entry_last_house_number_street_village.grid(row=6, column=2)
+        
+        self.last_barangay = tk.Label(self.user_information_window, text="Barangay:", bg="#ECDADD")
+        self.last_barangay.grid(row=6, column=3, padx=5, pady=10)
+        self.entry_last_barangay = tk.Entry(self.user_information_window)
+        self.entry_last_barangay.grid(row=6, column=4)
+        
+        self.last_city = tk.Label(self.user_information_window, text="City/Town:", bg="#ECDADD")
+        self.last_city.grid(row=6, column=5, padx=5, pady=10)
+        self.entry_last_city = tk.Entry(self.user_information_window)
+        self.entry_last_city.grid(row=6, column=6)
+    
+    
+        
