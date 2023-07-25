13b8bced914792202a86af3191786a0b3aeea2ad
ChristianJames Luces <christianjames231103@gmail.com>
2023-07-25 17:43:34 +0800

    add remove button

--- file_operations.py
+++ file_operations.py
@@ -11,7 +11,7 @@ class FileHandling():
     def search_window(self):
         finder_window = tk.Toplevel()
         finder_window.title("Search Data")
-        finder_window.geometry("800x400")
+        finder_window.geometry("800x300")
         finder_window.resizable(False, False)
 
 
@@ -23,11 +23,12 @@ class FileHandling():
         self.search_entry.pack(pady=10)
 
         # Create search result display
-        search_result = tk.Listbox(finder_window, width=700, height=250)
+        search_result = tk.Listbox(finder_window, width=700)
         search_result.pack(pady=10)
 
         self.data_button(finder_window)
         self.update_button(finder_window)
+        self.remove_button(finder_window)
 
     #Create button to search data
     def data_button(self, finder_window):
@@ -40,15 +41,20 @@ class FileHandling():
 
     #Create button to update data
     def update_button(self, finder_window):
-        update_buton = tk.Button(finder_window, text="Update",fg="green", font=("Time", 11, "bold"), command=self.update)
-        update_buton.place(x=700, y=10)
+        update_buton = tk.Button(finder_window, text="Update",fg="green", font=("Time", 11, "bold"), command=self.remove)
+        update_buton.place(x=700, y=250)
+
+    #Create button to remove data
+    def remove_button(self, finder_window):
+        remove_button = tk.Button(finder_window, text="Remove",fg="red", font=("Time", 11, "bold"), command=self.update)
+        remove_button.place(x=40, y=250)
 
 
     #Saving the user's data
     def search_data(self):
         search_keyword = self.search_entry.get()
         results = []
-        with open("user_database.csv", "r", newline="", encoding="utf-8") as file:
+        with open("contact_list.csv", "r", newline="", encoding="utf-8") as file:
             csv_reader = csv.reader(file)
             for row in csv_reader:
                 if search_keyword.lower() in " ".join(row).lower():
@@ -57,10 +63,10 @@ class FileHandling():
 
     #Saving the data if the user updates it
     def update(self, old_contact_data, new_contact_data):
-        with open("user_database.csv", "r", newline="", encoding="utf-8") as file:
+        with open("contact_list.csv", "r", newline="", encoding="utf-8") as file:
             data = list(csv.reader(file))
 
-        with open("user_database.csv", "w", newline="", encoding="utf-8") as file:
+        with open("contact_list.csv", "w", newline="", encoding="utf-8") as file:
             writer = csv.writer(file)
             updated = False
             for row in data:
@@ -74,4 +80,25 @@ class FileHandling():
                 messagebox.showinfo("Success", "Data updated successfully.")
             else:
                 messagebox.showwarning("Not Found", "Data not found for updating.")
+    
+
+    #Removing data 
+    def remove(self, contact_data):
+        with open("user_database.csv", "r", newline="", encoding="utf-8") as file:
+            data = list(csv.reader(file))
+
+        with open("user_database.csv", "w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            removed = False
+            for row in data:
+                if ", ".join(contact_data) != ", ".join(row):
+                    writer.writerow(row)
+                else:
+                    removed = True
+
+            if removed:
+                messagebox.showinfo("Success", "Data removed successfully.")
+            else:
+                messagebox.showwarning("Not Found", "Data not found for removal.")
+
 
